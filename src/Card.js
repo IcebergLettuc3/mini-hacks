164a472d331f765611d474c127f1bc7fb7634e55
Jonathan Eisenbrandt <jeisenbrandt1@gmail.com>
2024-01-25 20:00:42 -0600

    adding title

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Card = ({ movie }) => {
+const Card = ({ movie , onCardClick}) => {
     const IMAGE_BASE_URL = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
     return (
-        <div>
-            <Link to = '/'>
-                <div className="shadow-md rounded-lg p-4">
-                    <img src={IMAGE_BASE_URL + movie.poster_path} alt="" className="w-full h-46 object-cover rounded-lg" />
-                    <div className="mt-4">
-                        <h5 className=" text-black text-2xl font-semibold">{movie.title}</h5>
-                        <p className="text-gray-600 overflow-auto h-20">{movie.overview}</p>
-                    </div>
-                </div>
-            </Link>
+        <div className="shadow-md rounded-lg p-4" onClick={onCardClick}>
+            <img src={IMAGE_BASE_URL + movie.poster_path} alt="" className="w-full h-46 object-cover rounded-lg" />
+            <div className="mt-4">
+                <h5 className=" text-black text-2xl font-semibold">{movie.title}</h5>
+                <p className="text-gray-600 overflow-auto h-20">{movie.overview}</p>
+            </div>
         </div>
     );
 };
--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,20 @@ import { getTrendingMovies } from './utils/request';
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
-    var selectedMovie = useState([])
+    const [selectedTitle, setSelectedTitle] = useState('')
+    const [selectedId, setSelectedId] = useState(null)
 
-    useState(() => {
-        selectedMovie = async () => {
-            
-        }
-    })
 
     useEffect(() => {
         const fetchMovies = async () => {
             const trendingMovies = await getTrendingMovies();
             setMovies(trendingMovies);
         };
+    
+    const handleCardClick = (movie) => {
+        setSelectedTitle(movie.title)
+        setSelectedId(movie.id)
+    }
 
         fetchMovies();
     }, []);
@@ -24,11 +25,14 @@ const Home = () => {
     return (
         <main className="flex bg-white min-h-screen flex-col items-center justify-between p-24">
             <h1 className='flex text-black text-2xl font-semibold'>Top Trending Movies</h1>
-            <h2 className='flex text-black text-xl font-semibold'> Selected Movie: {selectedMovie}</h2>
+            <h2 className='flex text-black text-xl font-semibold'> Selected Movie: {selectedTitle}</h2>
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {movies.map(movie => {
-                        return <Card key={movie.id} movie={movie}/>
+                        return <Card
+                                    key={movie.id}
+                                    movie={movie}
+                                    onCardClick = {() => handleCardClick(movie)}/>
                     })}
                 </div>
             </div>
